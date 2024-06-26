3c387e3f23d63dba663359dc29c5e1106d6e1466
[Martin Carignan] <[mcarign1@uncc.edu]>
2024-06-26 11:53:46 -0400

    created a button in listPanel

--- GUI/MainGUI.java
+++ GUI/MainGUI.java
@@ -4,30 +4,34 @@ import com.sun.source.tree.UsesTree;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.ImageObserver;
+import java.util.LinkedList;
 
-public class MainGUI {
+public class MainGUI implements ActionListener {
     public static JFrame frame;
-    public static JPanel mainPanel;
     public static JPanel aesPanel;
     public static JPanel listPanel;
     public static JButton addEvent;
     public static JButton editEvent;
     public static JButton saveEvent;
-    public static JButton[] eventList;
+    public static ButtonGroup bg;
+    public static LinkedList<JToggleButton> eventList;
     
     public static void main(String[] args) {
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-        frame.setBounds(600, 300, 620, 650);
+        frame.setLocation(600, 300);
+        frame.setMinimumSize(new Dimension(600, 700));
 
         aesPanel = new JPanel();
-        aesPanel.setBounds(0, 0, 90, frame.getHeight() - 38);
-        aesPanel.setBackground(Color.gray);
         aesPanel.setLayout(new GridLayout(3, 1));
 
         listPanel = new JPanel();
-        listPanel.setBounds(aesPanel.getWidth(), 0, 530, frame.getHeight() - 38);
+        listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
+        listPanel.setBackground(Color.gray);
 
         addEvent = new JButton("Add Event");
         addEvent.setBackground(Color.PINK);
@@ -45,8 +49,28 @@ public class MainGUI {
         aesPanel.add(editEvent);
         aesPanel.add(saveEvent);
 
-        frame.add(aesPanel);
-        frame.add(listPanel);
+        bg = new ButtonGroup();
+        // Add a reader to see if user has saved activities for the day
+        eventList = new LinkedList<>();
+        JToggleButton toggle = new JToggleButton("hi");
+        toggle.setAlignmentX(Component.CENTER_ALIGNMENT);
+        toggle.setPreferredSize(new Dimension(500, 70));
+        toggle.setPreferredSize(new Dimension(500, 70));
+        eventList.push(toggle);
+        for(JToggleButton tg : eventList){
+            bg.add(tg);
+            listPanel.add(tg);
+        }
+
+        frame.add(aesPanel, BorderLayout.WEST);
+        frame.add(listPanel, BorderLayout.CENTER);
+        frame.pack();
         frame.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
